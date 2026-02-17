1d72e718cdd5ab967bc6a1e3f2b53bfd0c68bf41
Maurice Yang <mauriceyangmy28@gmail.com>
2026-02-18 03:38:24 +0700

    feat: added seeding during initial run

--- ECommerceApplication/src/main/java/com/app/ECommerceApplication.java
+++ ECommerceApplication/src/main/java/com/app/ECommerceApplication.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import com.app.config.AppConstants;
 import com.app.entites.Role;
 import com.app.repositories.RoleRepo;
+import com.app.services.SeedService;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -23,6 +24,9 @@ public class ECommerceApplication implements CommandLineRunner {
 
 	@Autowired
 	private RoleRepo roleRepo;
+
+	@Autowired
+	private SeedService seedService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
@@ -36,19 +40,8 @@ public class ECommerceApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		try {
-			Role adminRole = new Role();
-			adminRole.setRoleId(AppConstants.ADMIN_ID);
-			adminRole.setRoleName("ADMIN");
-
-			Role userRole = new Role();
-			userRole.setRoleId(AppConstants.USER_ID);
-			userRole.setRoleName("USER");
-
-			List<Role> roles = List.of(adminRole, userRole);
-
-			List<Role> savedRoles = roleRepo.saveAll(roles);
-
-			savedRoles.forEach(System.out::println);
+			String result = seedService.seedAll();
+			System.out.println(result);
 
 		} catch (Exception e) {
 			e.printStackTrace();
